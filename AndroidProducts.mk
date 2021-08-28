a3bde89170c5f3aa188e90560d3a121d5e03d9b3
David <davidmolinamorales1999@gmail.com>
2021-08-28 10:50:51 +0200

    typo

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_guacamoleb.mk
     
     COMMON_LUNCH_CHOICES := \
-    lineage_honami-user \
-    lineage_honami-userdebug \
-    lineage_honami-eng
+    lineage_guacamoleb-user \
+    lineage_guacamoleb-userdebug \
+    lineage_guacamoleb-eng
